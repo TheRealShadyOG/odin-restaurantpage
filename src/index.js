980d954ca1b8a4130571ee5ff81f9e1490011166
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-09 18:06:00 -0500

    Add functions to make tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import { loadHomePage } from "./homePage";
+import { loadMenuPage } from "./menuPage";
 
 loadHomePage();
+
+const body = document.querySelector('body');
+const content = document.querySelector('#content');
+
+
+
+function loadMenu() {
+    body.removeChild(body.lastElementChild);
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+    loadMenuPage();
+    setEventListeners();
+};
+
+function loadHome() {
+    body.removeChild(body.lastElementChild);
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+    loadHomePage();
+    setEventListeners();
+};
+
+function loadContact(){
+    body.removeChild(body.lastElementChild);
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+    // loadContactPage();
+    setEventListeners();
+}
+
+
+function setEventListeners() {
+    const homeButton = document.querySelector('.homeButton');
+    homeButton.addEventListener('click', loadHome);
+
+    const menuButtonHeader = document.querySelector('.menuButtonHeader');
+    menuButtonHeader.addEventListener('click', loadMenu);
+
+    const menuButton = document.querySelector('#menu');
+    menuButton.addEventListener('click', loadMenu);
+
+    const contactButton = document.querySelector('.contactButton');
+    contactButton.addEventListener('click', loadContact)
+}
+
+setEventListeners();
